5e11eaf101bd04f5c0d749bda35bff5e50e66adb
skirubame <skirubame@gmail.com>
2021-08-11 11:41:18 +0530

    Create ldr.ino

---
+++ ldr.ino
@@ -0,0 +1,34 @@
+int threshold = 600;
+
+// Connect the LED to digital pin 9
+int led = 9;
+
+// The photoresistor lead is connected to analog pin 0
+int sensor = A0;
+// stores the analog reading value
+int sensorValue = 0;
+
+void setup() {
+  // sets the LED as an output
+  pinMode(led, OUTPUT);
+  // sets the photoresistor as an input
+  pinMode(sensor, INPUT);
+  // starts the serial communication at a baud rate of 9600
+  Serial.begin(9600);
+}
+
+void loop(){
+  // Reads the current photoresistor value
+  sensorValue = analogRead(sensor);
+  // if the value is below a certain threshold it turns the LED on, otherwise the LED remains off
+  if (sensorValue < threshold) {
+      digitalWrite(led, HIGH);
+  }
+  else {
+      digitalWrite(led,LOW);
+  }
+  // prints the current readings in the Arduino IDE serial monitor
+  Serial.print ("Sensor reading: ");
+  Serial.println(sensorValue);
+  delay(50);
+}
