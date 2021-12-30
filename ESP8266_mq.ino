c9bb13a054c38aa7801d2eef7609ddf3838bb949
skirubame <skirubame@gmail.com>
2021-12-30 14:30:25 +0530

    Update ESP8266_mq.ino

--- ESP8266_mq.ino
+++ ESP8266_mq.ino
@@ -1,79 +1,64 @@
 #include <ESP8266WiFi.h>
 #include <MQTT.h>
-#include <dht.h>
-#define dht_apin D1  
-const char ssid[] = "";
-const char pass[] = "";
-dht DHT;
-               
+const char ssid[] = "c";
+const char pass[] = "66666666";
+int value,ldr;            
 // Initialize DHT sensor.
 //DHT dht(dht_apin, DHTTYPE);                
-
-float Temperature;
-float Humidity;
 WiFiClient net;
 MQTTClient client;
 
 unsigned long lastMillis = 0;
 
 void connect() {
-  Serial.print("checking wifi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(1000);
-  }
+
 
   Serial.print("\nconnecting...");
-  while (!client.connect("arduino", "try", "try")) {
+  while (!client.connect("kpriet", "try", "try")) {
     Serial.print(".");
     delay(1000);
   }
 
   Serial.println("\nconnected!");
-
-//client.subscribe("TemP");
 client.subscribe("HumI");
+client.subscribe("put");
+
 //messageReceived(
   // client.unsubscribe("/hello");
 }
 
 void messageReceived(String &topic, String &payload) {
   Serial.println("incoming: " + topic + " - " + payload);
-  
-  analogWrite(D2,payload.toInt());
+  value=payload.toInt();
+  Serial.println(value);
 }
 
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, pass);
- pinMode(dht_apin, INPUT);
+ pinMode(A0, INPUT);
  pinMode(D2,OUTPUT);
 
   // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
   // You need to set the IP address directly.
-  client.begin("broker.shiftr.io", net);
+  client.begin("broker.hivemq.com", net);
   client.onMessage(messageReceived);
 
   connect();
 }
 
 void loop() {
-   DHT.read11(dht_apin);
-  Temperature = DHT.temperature; // Gets the values of the temperature
-  Humidity = DHT.humidity; // Gets the values of the humidity 
+   
+  
+  ldr =analogRead(A0); // Gets the values of the humidity 
  // Serial.println(Temperature);
   //Serial.println(Humidity);
+  digitalWrite(D2,value);
   client.loop();
-  delay(10);  // <- fixes some issues with WiFi stability
-
-  if (!client.connected()) {
-    connect();
-  }
-  // publish a message roughly every second.
+  delay(10);  
   if (millis() - lastMillis > 1000) {
     lastMillis = millis();
-    client.publish("TemP", (String)Temperature);
-client.publish("HumI", (String)Humidity);
+    client.publish("LDR", (String)ldr);
     
   }
 }
